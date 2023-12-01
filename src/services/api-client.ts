3a73df09a21f8d3a62c1529e36d76068d7fea159
mcadena05 <cadenamarlynn@gmail.com>
2023-12-01 16:10:54 -0700

    API Key management

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: 'cfb7ff3c97ce488295a06afaec4b2cf9'
-
+        key: import.meta.env.VITE_SOME_KEY
     }
 })
