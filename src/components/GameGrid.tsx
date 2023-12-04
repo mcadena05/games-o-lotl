99f9d7a672d88b5cc7289d50287eb599d385ac5c
mcadena05 <cadenamarlynn@gmail.com>
2023-12-04 13:50:01 -0700

    Removed Duplicated Styles for Cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GarmeCardSkelecton from "./GarmeCardSkelecton";
+import GameCardContainer from "./GameCardContainer";
 
 
 
@@ -14,9 +15,9 @@ const GameGrid = () =>{
         <>
         {error && <Text>{error}</Text>}
         <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl:5}} padding={10} spacing={10}>
-            {isLoading && skelectons.map(skelecton => <GarmeCardSkelecton key={skelecton}></GarmeCardSkelecton>)}
+            {isLoading && skelectons.map(skelecton => <GameCardContainer><GarmeCardSkelecton key={skelecton}></GarmeCardSkelecton></GameCardContainer>)}
             {games.map((game) => (
-                <GameCard key={game.id} game={game}></GameCard>
+                <GameCardContainer><GameCard key={game.id} game={game}></GameCard></GameCardContainer>
             ))}
         </SimpleGrid>
         </>
--- src/components/GarmeCardSkelecton.tsx
+++ src/components/GarmeCardSkelecton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const GarmeCardSkelecton = () => {
   return (
-    <Card width='350px'>
+    <Card >
         <Skeleton height='200px' />
 
         <CardBody>
