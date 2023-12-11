0cba7abc0370e9fa5b5fed862c2a068227de5d1a
mcadena05 <cadenamarlynn@gmail.com>
2023-12-11 16:43:42 -0700

    Build Search Inputing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,18 +2,15 @@ import { Box, Flex, HStack, Heading, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
 
 export const NavBar = () => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
-      <Flex>
-        <Image src={logo} boxSize='60px' />
-        <Box marginTop={4}><Heading color='pink' size='md'>Games-O-Lotl</Heading></Box>
-      </Flex>
-      
-      
-       <ColorModeSwitch/> 
+    <HStack padding='10px'>
+      <Image src={logo} boxSize='60px' />
+      <SearchInput  ></SearchInput>
+      <ColorModeSwitch/> 
     </HStack>
   )
 }
