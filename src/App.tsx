022038750133e381d0b2be65233ff59eccfaca2a
mcadena05 <cadenamarlynn@gmail.com>
2023-12-11 17:23:18 -0700

    Dynamic filtering heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Genre } from './hooks/useGenres'
 import PlatformSelector from './components/PlatformSelector'
 import { Platform } from './hooks/useGames'
 import SortSelector from './components/SortSelector'
+import GameHeading from './components/GameHeading'
 
 export interface GameQuery{
   genre: Genre | null;
@@ -36,6 +37,7 @@ function App() {
         <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre})}/>
       </GridItem></Show>
       <GridItem area='main'>
+        <GameHeading gameQuery={gameQuery}></GameHeading>
         <Flex paddingLeft={10} marginBottom={2}>
           <Box marginRight={5}>
             <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}></PlatformSelector>
